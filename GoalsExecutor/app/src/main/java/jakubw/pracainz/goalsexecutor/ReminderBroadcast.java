39a1fb829b27b737c542085f0f9724d2c1d851ee
ma7inowy <jakubwachala@wp.pl>
2020-08-29 12:45:22 +0200

    Added on notification click handler

--- GoalsExecutor/app/src/main/java/jakubw/pracainz/goalsexecutor/ReminderBroadcast.java
+++ GoalsExecutor/app/src/main/java/jakubw/pracainz/goalsexecutor/ReminderBroadcast.java
@@ -1,5 +1,6 @@
 package jakubw.pracainz.goalsexecutor;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -11,10 +12,15 @@ public class ReminderBroadcast extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         String desc = intent.getStringExtra("desc");
+        //call when i tap on notification
+        Intent intent2 = new Intent(context,MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(context,0,intent2,0);
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context,"notifyGoalsExecutor")
                 .setSmallIcon(R.drawable.ic_message)
                 .setContentTitle("Daily Tasks! Do it!")
                 .setContentText("Masz zrobic zadanko: " + desc)
+                .setContentIntent(contentIntent)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
