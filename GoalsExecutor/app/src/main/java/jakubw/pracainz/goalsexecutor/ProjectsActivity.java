e9adfbfc6c684fa9bae8a9e754414d1f2af33165
ma7inowy <jakubwachala@wp.pl>
2020-09-08 19:11:55 +0200

    In ProjectsActivity added on swipe left delete for recycler view

--- GoalsExecutor/app/src/main/java/jakubw/pracainz/goalsexecutor/ProjectsActivity.java
+++ GoalsExecutor/app/src/main/java/jakubw/pracainz/goalsexecutor/ProjectsActivity.java
@@ -1,5 +1,6 @@
 package jakubw.pracainz.goalsexecutor;
 
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -7,12 +8,15 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,7 +24,12 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
+
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
+
+import static androidx.recyclerview.widget.ItemTouchHelper.LEFT;
 
 public class ProjectsActivity extends AppCompatActivity implements ProjectsAdapter.OnNoteListener {
 
@@ -73,6 +82,9 @@ public class ProjectsActivity extends AppCompatActivity implements ProjectsAdapt
             }
         });
 
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(myProjects);
+
     }
 
     public void setAdapter(ArrayList<Project> projectList) {
@@ -91,4 +103,51 @@ public class ProjectsActivity extends AppCompatActivity implements ProjectsAdapt
         NewProjectDialog newProjectDialog = new NewProjectDialog();
         newProjectDialog.show(getSupportFragmentManager(),"Example");
     }
+
+    // swipe left to delete task
+    ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, LEFT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            final int postion = viewHolder.getAdapterPosition();
+
+            switch (direction) {
+                case LEFT:
+                    final Project deletedTask = projectList.get(postion);
+
+                    String id = deletedTask.getId();
+                    reference.child("Project" + id).removeValue(); // usuwa z bazy
+                    projectList.remove(postion);
+                    setAdapter(projectList);
+                    Snackbar.make(myProjects, "Project " + deletedTask.getTitle() + " deleted!", Snackbar.LENGTH_LONG).setAction("Undo", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            projectList.add(postion, deletedTask);
+                            setAdapter(projectList);
+                            HashMap map = new HashMap();
+                            map.put("title", deletedTask.getTitle());
+                            map.put("id", deletedTask.getId());
+                            reference.child("Project" + deletedTask.getId()).updateChildren(map);
+                        }
+                    }).show();
+                    break;
+            }
+        }
+
+        @Override
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+            new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                    .addSwipeLeftBackgroundColor(ContextCompat.getColor(ProjectsActivity.this, R.color.colorDeleteTask))
+//                    .addBackgroundColor(ContextCompat.getColor(KontenerActivity.this, R.color.colorPrimary))
+                    .addSwipeLeftActionIcon(R.drawable.ic_delete_black_)
+                    .create()
+                    .decorate();
+
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+    };
 }
