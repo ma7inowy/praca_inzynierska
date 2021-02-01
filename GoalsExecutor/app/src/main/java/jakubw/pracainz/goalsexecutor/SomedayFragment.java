5f77af54c4a55032dd18709421a50edbdf27fcba
ma7inowy <jakubwachala@wp.pl>
2021-02-01 19:11:00 +0100

    In SomedayFragment added on swipe to delete method

--- GoalsExecutor/app/src/main/java/jakubw/pracainz/goalsexecutor/SomedayFragment.java
+++ GoalsExecutor/app/src/main/java/jakubw/pracainz/goalsexecutor/SomedayFragment.java
@@ -3,10 +3,13 @@ package jakubw.pracainz.goalsexecutor;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,6 +20,7 @@ import android.widget.Toast;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,10 +28,14 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
 import jakubw.pracainz.goalsexecutor.Model.Project;
 import jakubw.pracainz.goalsexecutor.Model.Someday;
+
+import static androidx.recyclerview.widget.ItemTouchHelper.LEFT;
 
 public class SomedayFragment extends Fragment implements SomedayAdapter.OnItemListener{
 
@@ -85,6 +93,8 @@ public class SomedayFragment extends Fragment implements SomedayAdapter.OnItemLi
             }
         });
 
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerSomeday);
     }
 
     private void openDialogToAddNewSomedayTask() {
@@ -103,4 +113,49 @@ public class SomedayFragment extends Fragment implements SomedayAdapter.OnItemLi
         final Someday someday = somedayList.get(position);
         Toast.makeText(getContext(), "id" + someday.getId(), Toast.LENGTH_SHORT).show();
     }
+
+    ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, LEFT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            final int postion = viewHolder.getAdapterPosition();
+
+            switch (direction) {
+                case LEFT:
+                    final Someday deletedTask = somedayList.get(postion);
+
+                    String id = deletedTask.getId();
+                    reference.child("Someday" + id).removeValue(); // usuwa z bazy
+                    somedayList.remove(postion);
+                    setAdapter(somedayList);
+                    Snackbar.make(recyclerSomeday, "Someday " + deletedTask.getTitle() + " deleted!", Snackbar.LENGTH_LONG).setAction("Undo", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            somedayList.add(postion, deletedTask);
+                            setAdapter(somedayList);
+                            HashMap map = new HashMap();
+                            map.put("title", deletedTask.getTitle());
+                            map.put("id", deletedTask.getId());
+                            reference.child("Someday" + deletedTask.getId()).updateChildren(map);
+                        }
+                    }).show();
+                    break;
+            }
+        }
+
+        @Override
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+            new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                    .addSwipeLeftBackgroundColor(ContextCompat.getColor(getActivity(), R.color.colorDeleteTask))
+                    .addSwipeLeftActionIcon(R.drawable.ic_delete_black_)
+                    .create()
+                    .decorate();
+
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+    };
 }
